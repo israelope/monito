29d9a3ac5d6917c154a1f0a01392524cdad8d784
Oloyede Ola <israeloloyede002@gmail.com>
2025-10-11 23:44:50 +0100

    Push first files

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+  import SearchBar from './Search'
+
+const App = () => {
+  return (
+    <>
+    <div className='bg-green-500 h-screen'>App</div>
+    <SearchBar/>
+    </>
+  )
+}
+
+export default App
